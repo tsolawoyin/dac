255848c20ee8d7d1942bf7164fb40c9aff1bf1ca
tsolawoyin <t.olawoyin@outlook.com>
2026-04-25 11:40:16 +0100

    Update questions

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -62,9 +62,40 @@ export const topics: Topic[] = [
     order: 4,
   },
   {
+    id: "blood-composition-and-functions",
+    name: "Blood - Composition and Functions",
+    subject: "physiology-101",
+    order: 5,
+  },
+  {
+    id: "red-blood-cells-and-erythropoiesis",
+    name: "Blood - RBC and Erythropoiesis",
+    subject: "physiology-101",
+    order: 6,
+  },
+  {
+    id: "white-blood-cells-and-immunity",
+    name: "Blood - WBC and Immunity",
+    subject: "physiology-101",
+    order: 7,
+  },
+  {
+    id: "hemostasis-and-platelets",
+    name: "Blood - Homeostasis and Platelets",
+    subject: "physiology-101",
+    order: 8,
+  },
+  {
+    id: "blood-groups-and-clinical",
+    name: "Blood - Groups and clinicals",
+    subject: "physiology-101",
+    order: 9,
+  },
+
+  {
     id: "cardiovascular-system-001",
     name: "Cardiovascular system",
     subject: "physiology-101",
-    order: 5,
+    order: 20,
   },
 ];
